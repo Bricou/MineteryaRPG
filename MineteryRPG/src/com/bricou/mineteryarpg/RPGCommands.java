80c4535e2ee89b36451ccc049b6ce4e1ec591a70
Bricou <brice.sevat@gmail.com>
2011-07-28 14:33:49 +0200

    commit de reprise de boulot

--- MineteryRPG/src/com/bricou/mineteryarpg/RPGCommands.java
+++ MineteryRPG/src/com/bricou/mineteryarpg/RPGCommands.java
@@ -14,11 +14,25 @@ public class RPGCommands {
 	
 	public boolean handleCommand(String command, Player player, String[] args)
 	{
-		if (command.equalsIgnoreCase("rpghelp")) {
-			// Pas besoin de vérifier le nombre d'arguments
-			player.sendMessage(ChatColor.AQUA + "[MineteryaRPG] Aide");
-			player.sendMessage(ChatColor.AQUA + "/rpghelp - Affiche ce menu");
-			return true;
+		if (command.equalsIgnoreCase("rpgaide"))
+		{
+			if (args.length == 0)
+			{
+				player.sendMessage(ChatColor.AQUA + "[MineteryaRPG] Aide");
+				player.sendMessage(ChatColor.AQUA + "/rpgaide - Affiche ce menu");
+				player.sendMessage(ChatColor.AQUA + "/rpgaide classes - Affiche les informations à propos des classes");
+				return true;
+			}
+			else if (args.length == 1)
+			{
+				if(args[0].equalsIgnoreCase("classes"))
+				{
+					player.sendMessage(ChatColor.AQUA + "[MineteryaRPG] Informations sur les classes");
+					player.sendMessage(ChatColor.AQUA + "Guerrier - Classe pouvant porter des coups puissants ou réduire les dégâts subis");
+					return true;
+				}
+				return false;
+			}
 		}
 		return false;
 	}
